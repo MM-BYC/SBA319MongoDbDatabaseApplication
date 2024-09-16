3b6098fb490daa0fd6cdeb2a53010d33eab10674
mmarque <marquezmusa@gmail.com>
2024-09-16 01:28:08 -0400

    add updatedAt createdAt datefield in user model

--- models/user.js
+++ models/user.js
@@ -5,8 +5,24 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   userName: String,
   userPassword: String,
-  userEmail: String,
+  userEmail: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+//--> indexed by userEmail, unique: true will ensure that email is unique
+userSchema.index({ userEmail: 1 }, { unique: true });
+
 //---> create the collection (Supply 3rd argument otherwise defaults to plural "users")
 const User = mongoose.model("User", userSchema, "usersCollection");
 
